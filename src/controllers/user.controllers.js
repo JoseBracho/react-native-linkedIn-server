9eefe4922434902a00250ebca922bd6afb7a6054
Jose <joseluisbracho0@gmail.com>
2021-03-28 17:11:10 -0400

    to be continue

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,7 +3,7 @@ const User = require("../model/user.model");
 
 const userInfo = async (req = request, res = response) => {
     const id = req.params.id;
-    const {user} = await User.findById(id)
+    const user = await User.findById(id)
     res.json(user);
 }
 const userUpdate = async (req = request, res = response) => {
